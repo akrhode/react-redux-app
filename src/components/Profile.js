be9b699e610a3ed5801d9e3c57de96f4f6747e33
akrhode <a.rhode@web.de>
2023-04-23 18:43:47 +0200

    Create reducer

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Profile() {
+  const user = useSelector((state) => state.user.value);
+
   return (
     <div>
       <h1>Profile Page</h1>
-      <p>Name:</p>
-      <p>Email:</p>
-      <p>Age:</p>
+      <p>Name:{user.name}</p>
+      <p>Email:{user.email}</p>
+      <p>Age:{user.age}</p>
     </div>
   );
 }
---
+++ src/features/user.js
@@ -0,0 +1,12 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name:"user",
+  initialState: {value: {name: "", email: "", age: 0}},
+  reducers: {
+    login: (state, action) => { state.value = action.payload},
+  }
+});
+
+export default userSlice.reducer;
+
