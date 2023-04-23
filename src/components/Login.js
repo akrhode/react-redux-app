64d4bea0c7a25f6685fff3201271a4659940af74
akrhode <a.rhode@web.de>
2023-04-23 21:06:26 +0200

    Implement createSlice and dispatch actions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
 
 function Login() {
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Laura", email: "Laura@gmail.com", age: 30 }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 }
--- src/features/user.js
+++ src/features/user.js
@@ -1,12 +1,21 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateValue = { name: "", email: "", age: 0 };
 
 export const userSlice = createSlice({
-  name:"user",
-  initialState: {value: {name: "", email: "", age: 0}},
+  name: "user",
+  initialState: { value: initialStateValue },
   reducers: {
-    login: (state, action) => { state.value = action.payload},
-  }
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+
+    logout: (state) => {
+      state.value = initialStateValue;
+    },
+  },
 });
 
-export default userSlice.reducer;
+export const { login, logout } = userSlice.actions;
 
+export default userSlice.reducer;
